c0b032ef96dd930b39f955e09bd0769ce83c852b
Roee Barzilai <roee159@gmail.com>
2024-03-08 21:06:48 +0200

    add fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Homepage from './components/Homepage'
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-
+         <Homepage></Homepage>
 
       </div>
     </div>
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+
+export default function Homepage(){
+
+    const [imageData, setImageData] = useState([]);
+
+  useEffect(() => {
+    // Fetch images from the picsum.photos API
+    const fetchImages = async () => {
+      try {
+        const response = await fetch('https://picsum.photos/v2/list?page=1&limit=5');
+        const data = await response.json();
+        setImageData(data);
+      } catch (error) {
+        console.error('Error fetching images:', error);
+      }
+    };
+
+    fetchImages();
+  }, []); // Empty dependency array to run the effect only once on component mount
+
+
+    return (
+        <div className="container mx-auto">
+          <div className="relative">
+            <div className="overflow-hidden">
+              <div className="flex -mx-2">
+                {imageData.map((image, index) => (
+                  <div key={index} className="w-full px-2">
+                    <img
+                      className="carousel-img object-cover w-full h-64 md:h-80 lg:h-96 xl:h-120"
+                      src={image.download_url}
+                      alt={`Background ${index + 1}`}
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+}
