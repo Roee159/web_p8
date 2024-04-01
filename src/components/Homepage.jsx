c54078c3ab5d89c5e35a066c36ced558a4625256
Roee Barzilai <roee159@gmail.com>
2024-04-01 23:19:26 +0300

    fix images size

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,42 +1,154 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from "react";
+import "./Homepage.css"; // Make sure to import your CSS file
+import { FaHandPointLeft, FaHandPointRight } from "react-icons/fa";
+function Homepage() {
+  const [isIconVisible, setIsIconVisible] = React.useState(false);
+  const [currentImage, setCurrentImage] = useState(
+    "https://picsum.photos/id/1/350/350"
+  );
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const images = [
+    {
+      id: 1,
+      url: "https://picsum.photos/id/1/350/350",
+      thumbnailUrl: "https://picsum.photos/id/1/50/50",
+    },
+    {
+      id: 2,
+      url: "https://picsum.photos/id/2/350/350",
+      thumbnailUrl: "https://picsum.photos/id/2/50/50",
+    },
+    {
+      id: 3,
+      url: "https://picsum.photos/id/3/350/350",
+      thumbnailUrl: "https://picsum.photos/id/3/50/50",
+    },
+    {
+      id: 4,
+      url: "https://picsum.photos/id/4/350/350",
+      thumbnailUrl: "https://picsum.photos/id/4/50/50",
+    },
+    {
+      id: 5,
+      url: "https://picsum.photos/id/5/350/350",
+      thumbnailUrl: "https://picsum.photos/id/5/50/50",
+    },
+    {
+      id: 6,
+      url: "https://picsum.photos/id/6/350/350",
+      thumbnailUrl: "https://picsum.photos/id/6/50/50",
+    },
+    {
+      id: 7,
+      url: "https://picsum.photos/id/7/350/350",
+      thumbnailUrl: "https://picsum.photos/id/7/50/50",
+    },
+    {
+      id: 8,
+      url: "https://picsum.photos/id/8/350/350",
+      thumbnailUrl: "https://picsum.photos/id/8/50/50",
+    },
+    {
+      id: 9,
+      url: "https://picsum.photos/id/9/350/350",
+      thumbnailUrl: "https://picsum.photos/id/9/50/50",
+    },
+    {
+      id: 10,
+      url: "https://picsum.photos/id/10/350/350",
+      thumbnailUrl: "https://picsum.photos/id/10/50/50",
+    },
+    {
+      id: 11,
+      url: "https://picsum.photos/id/11/350/350",
+      thumbnailUrl: "https://picsum.photos/id/11/50/50",
+    },
+    {
+      id: 12,
+      url: "https://picsum.photos/id/12/350/350",
+      thumbnailUrl: "https://picsum.photos/id/12/50/50",
+    },
+    {
+      id: 13,
+      url: "https://picsum.photos/id/13/350/350",
+      thumbnailUrl: "https://picsum.photos/id/13/50/50",
+    },
+    {
+      id: 14,
+      url: "https://picsum.photos/id/14/350/350",
+      thumbnailUrl: "https://picsum.photos/id/14/50/50",
+    },
+    {
+      id: 15,
+      url: "https://picsum.photos/id/15/350/350",
+      thumbnailUrl: "https://picsum.photos/id/15/50/50",
+    },
+    {
+      id: 16,
+      url: "https://picsum.photos/id/16/350/350",
+      thumbnailUrl: "https://picsum.photos/id/16/50/50",
+    },
+    {
+      id: 17,
+      url: "https://picsum.photos/id/17/350/350",
+      thumbnailUrl: "https://picsum.photos/id/17/50/50",
+    },
+    {
+      id: 18,
+      url: "https://picsum.photos/id/18/350/350",
+      thumbnailUrl: "https://picsum.photos/id/18/50/50",
+    },
+    {
+      id: 19,
+      url: "https://picsum.photos/id/19/350/350",
+      thumbnailUrl: "https://picsum.photos/id/19/50/50",
+    },
+    {
+      id: 20,
+      url: "https://picsum.photos/id/20/350/350",
+      thumbnailUrl: "https://picsum.photos/id/20/50/50",
+    },
+  ];
 
-export default function Homepage(){
+  const setImage = (index) => {
+    setCurrentImageIndex(index);
+ };
 
-    const [imageData, setImageData] = useState([]);
+ const nextImage = () => {
+    if (currentImageIndex < images.length - 1) {
+      setCurrentImageIndex(currentImageIndex + 1);
+    }
+    setIsIconVisible(oldState => !oldState);
+ };
 
-  useEffect(() => {
-    // Fetch images from the picsum.photos API
-    const fetchImages = async () => {
-      try {
-        const response = await fetch('https://picsum.photos/v2/list?page=1&limit=5');
-        const data = await response.json();
-        setImageData(data);
-      } catch (error) {
-        console.error('Error fetching images:', error);
-      }
-    };
+ const prevImage = () => {
+    if (currentImageIndex > 0) {
+      setCurrentImageIndex(currentImageIndex - 1);
+    }
+    setIsIconVisible(oldState => !oldState);
+ };
 
-    fetchImages();
-  }, []); // Empty dependency array to run the effect only once on component mount
-
-
-    return (
-        <div className="container mx-auto">
-          <div className="relative">
-            <div className="overflow-hidden">
-              <div className="flex -mx-2">
-                {imageData.map((image, index) => (
-                  <div key={index} className="w-full px-2">
-                    <img
-                      className="carousel-img object-cover w-full h-64 md:h-80 lg:h-96 xl:h-120"
-                      src={image.download_url}
-                      alt={`Background ${index + 1}`}
-                    />
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
-      );
+ return (
+    <div className="wrapper">
+      <h1>Lab 8 - Web: Image Browser - React.JS</h1>
+      <div className="small-images">
+        {images.map((image, index) => (
+          <img
+            key={image.id}
+            src={image.thumbnailUrl}
+            onClick={() => setImage(index)}
+            className="img-thumbnail"
+          />
+        ))}
+      </div>
+      <div className="image-navigation">
+        <FaHandPointLeft size={40} onClick={prevImage} disabled={currentImageIndex === 0}/>
+        <img src={images[currentImageIndex].url} className="img-fluid" alt="Selected Image" />
+        <FaHandPointRight size={40} onClick={nextImage} disabled={currentImageIndex === images.length - 1}/>
+      </div>
+      
+    </div>
+ );
 }
+
+export default Homepage;
